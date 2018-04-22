a1c3154df8a2cb3285597519c236645c4a39b7db
Stephen Hellicar <shellicar@gmail.com>
2018-04-22 13:43:47 +1000

    Cleanup.

--- FluentFixture/ITestDefinition.cs
+++ FluentFixture/ITestDefinition.cs
@@ -8,12 +8,11 @@ namespace FluentFixture
         Func<object> Execute();
     }
 
-
     public interface ITestDefinition<out TFixture> : ITestDefinition
     {
         ITestDefinition<TFixture> Invoke(TestAction<TFixture> action);
         ITestDefinition<TFixture> InvokeWithResult(TestActionWithResult<TFixture> action);
-        ITestDefinition<TFixture> InvokeWithResult<TResult>(TestFunctionWithResult<TFixture, TResult> func);
         ITestDefinition<TFixture> Invoke<TResult>(TestFunction<TFixture, TResult> func);
+        ITestDefinition<TFixture> InvokeWithResult<TResult>(TestFunctionWithResult<TFixture, TResult> func);
     }
 }
